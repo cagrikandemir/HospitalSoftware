29ceb2e0f0951d9702926bb3f20b96d940bc7841
cagri kandemir <cagrikandemir58@hotmail.com>
2024-01-08 22:27:29 +0300

    Son Hali.

--- HastaneOtomasyon2/Program.cs
+++ HastaneOtomasyon2/Program.cs
@@ -16,7 +16,7 @@ namespace HastaneOtomasyon2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
+            Application.Run(new Form1());
         }
     }
 }
